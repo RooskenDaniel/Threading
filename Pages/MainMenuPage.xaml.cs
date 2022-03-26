39d658038c846b842ef11a663c93a2e696dee6bc
BramB123 <23478245+BramB123@users.noreply.github.com>
2022-03-26 19:16:12 +0100

    Add keyboard controls to main menu

--- Pages/MainMenuPage.xaml.cs
+++ Pages/MainMenuPage.xaml.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using Microsoft.Toolkit.Uwp.UI.Controls;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.System;
 using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
@@ -16,13 +18,15 @@ using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
-namespace Tetris
+namespace Tetris.Pages
+
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
     public sealed partial class MainMenuPage : Page
     {
+        private int? FocusedRow = 0;
         public MainMenuPage()
         {
             this.InitializeComponent();
@@ -30,7 +34,7 @@ namespace Tetris
 
         void NewGameButton_Click(object sender, RoutedEventArgs e)
         {
-            
+            //this.Frame.Navigate(typeof(GamePage));
         }
 
         void ReplayButton_Click(object sender, RoutedEventArgs e)
@@ -56,6 +60,55 @@ namespace Tetris
                 view.TryEnterFullScreenMode();
                 ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.FullScreen;
             }
+        }
+
+        private void Page_KeyDown(object sender, KeyRoutedEventArgs e)
+        {
+            int newValue;
+            switch (e.Key)
+            {
+                case VirtualKey.S:
+                case VirtualKey.Down:
+                case VirtualKey.GamepadDPadDown:
+                case VirtualKey.GamepadLeftThumbstickDown:
+                    UniformGrid grid = this.FindName("ButtonGrid") as UniformGrid;
+                    if (FocusedRow != null)
+                    {
+                        newValue = FocusedRow.Value + 1;
+                        if (newValue < grid.Rows)
+                        {
+                            FocusedRow = newValue;
+                        }
+                    }
+                    setFocus();
+                    break;
+                case VirtualKey.W:
+                case VirtualKey.Up:
+                case VirtualKey.GamepadDPadUp:
+                case VirtualKey.GamepadLeftThumbstickUp:
+                    if (FocusedRow != null)
+                    {
+                        newValue = FocusedRow.Value - 1;
+                        if (newValue >= 0)
+                        {
+                            FocusedRow = newValue;
+                        }
+                    }
+                    setFocus();
+                    break;
+                default: break;
+            }
+        }
+
+        private void setFocus()
+        {
+            UniformGrid grid = this.FindName("ButtonGrid") as UniformGrid;
+            if (FocusedRow == null)
+            {
+                FocusedRow = 0;
+            }
+            Button b = grid.Children[FocusedRow.Value] as Button;
+            b.Focus(FocusState.Keyboard);
         }
     }
 }
