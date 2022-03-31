ae04c3ea08d1e65180881d627cc1f5bf2457e8dc
dennisjhuu <dennisrozema1998@gmail.com>
2022-03-31 13:13:05 +0200

    replaymanager fix tasks and methods

--- Models/ReplayManager.cs
+++ Models/ReplayManager.cs
@@ -9,15 +9,27 @@ namespace Tetris.Models
 {
     class ReplayManager
     {
-        public async Task loadFromFile(string path)
+        public async void loadFromFile(string path)
+        {
+            Task task = new Task (() => loadFromFileTask(path));
+        }
+
+        public async void writeToFile(string fileName, string userInput)
+        {
+            Task task = new Task (() => writeToFileTask(fileName, userInput));
+        }
+
+        public async Task loadFromFileTask(string path)
         {
             string text = await File.ReadAllTextAsync(path);
         }
 
         public async Task writeToFileTask(string fileName, string userInput)
         {
+            string timestamp = System.DateTime.Now;
+            string fileText = userInput + " : " + userInput;
             using StreamWriter streamWriter = new(fileName, append: true);
-            await streamWriter.WriteLineAsync(userInput);
+            await streamWriter.WriteLineAsync(fileText);
         }
     }
 }
