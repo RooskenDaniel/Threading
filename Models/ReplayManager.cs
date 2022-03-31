17fa5f9c50ca0dd951e4968ebd551c8c6e53a1b2
dennisjhuu <dennisrozema1998@gmail.com>
2022-03-31 10:47:37 +0200

    Create replayManager

---
+++ Models/ReplayManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris.Models
+{
+    class ReplayManager
+    {
+        public async Task loadFromFile(string path)
+        {
+            string text = await File.ReadAllTextAsync(path);
+        }
+
+        public async Task writeToFileTask(string fileName, string userInput)
+        {
+            using StreamWriter streamWriter = new(fileName, append: true);
+            await streamWriter.WriteLineAsync(userInput);
+        }
+    }
+}
